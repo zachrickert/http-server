7f7984fe6c87cdcc2b4f49bfedce3ec3432ae1aa
James <canning.jimmy@gmail.com>
2016-08-16 15:01:11 -0700

    commit for driver change

--- src/client.py
+++ src/client.py
@@ -6,28 +6,27 @@ from sys import argv
 
 def client(message):
     buffer_length = 8
-
     client_socket = socket.socket(
         socket.AF_INET,
         socket.SOCK_STREAM,
         socket.IPPROTO_TCP
     )
-
-    client_socket.connect(('127.0.0.1', 5006))
-
-    # while True:
-    #     client_socket.listen(1)
-    #     conn, addr = client_socket.accept()
-    #     message_complete = False
-    #     echo_messsage = ""
-    #     while not message_complete:
-    #         part = conn.recv(buffer_length)
-    #         echo_messsage += (part.decode('utf8'))
-    #         if len(part) < buffer_length:
-    #             message_complete = True
-    #     print('this is the echo' + echo_messsage)
-
+    client_socket.connect(('127.0.0.1', 5015))
     client_socket.sendall(message.encode('utf8'))
+    address = ('127.0.0.1', 5016)
+    client_socket.bind(address)
+    while True:
+        client_socket.listen(1)
+        conn, addr = client_socket.accept()
+        message_complete = False
+        echo_message = ""
+        while not message_complete:
+            part = conn.recv(buffer_length)
+            echo_message += (part.decode('utf8'))
+            if len(part) < buffer_length:
+                message_complete = True
+        print('this is the echo' + echo_message)
+
 
 if __name__ == '__main__':
     client(argv[1])
--- src/server.py
+++ src/server.py
@@ -4,29 +4,26 @@ import socket
 
 def server():
     buffer_length = 8
-
     server_socket = socket.socket(
         socket.AF_INET,
         socket.SOCK_STREAM,
         socket.IPPROTO_TCP
     )
-    address = ('127.0.0.1', 5006)
+    address = ('127.0.0.1', 5015)
     server_socket.bind(address)
-
     while True:
         server_socket.listen(1)
         conn, addr = server_socket.accept()
         message_complete = False
-        recv_messsage = ""
+        recv_message = ""
         while not message_complete:
             part = conn.recv(buffer_length)
-            recv_messsage += (part.decode('utf8'))
+            recv_message += (part.decode('utf8'))
             if len(part) < buffer_length:
                 message_complete = True
-        print(recv_messsage)
-
-        # server_socket.sendall(recv_messsage.encode('utf8'))
-
+        print(recv_message)
+        server_socket.connect(('127.0.0.1', 5016))
+        server_socket.sendall(recv_message.encode('utf8'))
 
 if __name__ == '__main__':
     server()
